bda9ff888f6c8d974557acca473974defb42d2a2
Tomaz Canabrava <tcanabrava@kde.org>
2026-06-02 14:54:27 +0200

    Remove unsafe block, use byte-array instead of vec

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,20 @@
-fn maximum_odd_binary(s: &[u8], result: &mut Vec<u8>) {
+fn maximum_odd_binary(s: &[u8], result: &mut [u8]) {
     let len = s.len();
     let ones: usize = bytecount::count(s, b'1');
 
     if ones == 0 {
-        result.clear();
+        result.fill(b'0');
         return;
     }
 
-    unsafe {
-        result.set_len(0);
-        result.reserve(len);
-        let p: *mut u8 = result.as_mut_ptr();
-        std::ptr::write_bytes(p, b'1', ones - 1);
-        std::ptr::write_bytes(p.add(ones - 1), b'0', len - ones);
-        *p.add(len - 1) = b'1';
-        result.set_len(len);
-    }
+    result[..ones - 1].fill(b'1');
+    result[ones - 1..len - 1].fill(b'0');
+    result[len - 1] = b'1';
 }
 
 fn main() {
     let s = std::fs::read("large_binary.txt").expect("Failed to open large_binary.txt");
-    let mut result = Vec::with_capacity(s.len());
+    let mut result = vec![0u8; s.len()];
 
     for i in 0..5 {
         let start = std::time::Instant::now();
